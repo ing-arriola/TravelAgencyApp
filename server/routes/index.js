40972298f0caa794d97194f4895d321b6b1a2ad4
ing-arriola <ingeniero.arriola@gmail.com>
2020-04-11 10:39:45 -0600

    Added controller to homePage

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,34 +5,9 @@ const router = express.Router()
 const Travel = require('../models/Travels')
 const Opinion = require('../models/Opinions')
 const companyController = require('../controllers/companyController')
+const homeController = require('../controllers/homeController')
 module.exports = function(){
-    router.get('/',(req,res)=>{
-        const promises=[] //Array of promises in order to get data from two queries and pass the results to the view
-        
-        promises.push(
-            Travel.findAll({
-                limit: 3
-            })
-        )
-
-        promises.push(
-            Opinion.findAll({
-                limit: 3
-            })
-        )
-
-        //Execute the promises
-
-        const result=Promise.all(promises)
-        
-        result.then(result => res.render('index',{
-                page:"Cooming Soon",
-                clase:'home',
-                travels: result[0],
-                opinions: result[1]
-            }))
-            .catch(error => console.log(error))
-    })
+    router.get('/',homeController.queryToDB)
     
     router.get('/company',companyController.infoCompany)
 
