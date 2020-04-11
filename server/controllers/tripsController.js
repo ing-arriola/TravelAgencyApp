47fca09f4943f402e539339da2cdb4a513bf5dec
ing-arriola <ingeniero.arriola@gmail.com>
2020-04-11 14:38:26 -0600

    Added async await to trips controllers

--- server/controllers/tripsController.js
+++ server/controllers/tripsController.js
@@ -1,17 +1,15 @@
 const Travel = require('../models/Travels')
-exports.queryToTravels = (req,res)=>{
-    Travel.findAll()
-        .then(travels => res.render('trips',{
+exports.queryToTravels = async (req,res)=>{
+    const travels = await Travel.findAll()
+        res.render('trips',{
             page:"Cooming Soon",
             travels
-        }))
-        .catch(error => console.log(error))
+        })
     }
 
-exports.queryToTravel = (req,res)=>{
-    Travel.findByPk(req.params.id)
-        .then(travel => res.render('travel',{
-            travel
-        }))
-        .catch(error => console.log(error))
+exports.queryToTravel = async (req,res)=>{
+    const travel= await Travel.findByPk(req.params.id)
+    res.render('travel',{
+        travel
+    })
 }
